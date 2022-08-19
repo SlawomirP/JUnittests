b6f8c6aa00539a12f07be19e5be05962dc07c971
slawo <slawekpettke@gmail.com>
2022-08-19 10:22:19 +0200

    testy powiazane

---
+++ src/test/java/slaw/electricity/TwoElectrocityMetersTest.java
@@ -0,0 +1,53 @@
+package slaw.electricity;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class TwoElectrocityMetersTest { // zestaw testow
+
+//sprawdzamy czy doda wartosc, --przypadek testowy--
+    @Test
+    public void addKwh() {
+//nowy licznik -given
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+//dodanie -when
+        electricityMeter.addKwh(1);
+//sprawdzamy czy jest prawda -than
+        Assert.assertTrue(electricityMeter.getKwh() == 1);// trzeba dodac gettera
+
+        //Assert.assertTrue("powinno dac 1", electricityMeter.getKwh() == 1);
+    }
+
+
+    //testowanie wyjatku, pamietać o dodaniu expected
+    @Test (expected = ArithmeticException.class)
+    public void getHowMoreExpensiveNormalIs() {
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        electricityMeter.setCentsForKwh(90);
+        electricityMeter.getHowMoreExpensiveNormalIs();
+
+    }
+    //ignorowanie testu
+    @Ignore ("komunikat ktory pokaze sie jako komentarz przy tescie")
+    @Test
+    public void addKwh2() {
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        electricityMeter.addKwh(1);
+        Assert.assertTrue(electricityMeter.getKwh() == 1);// trzeba dodac gettera
+
+    }
+
+    //GWT GIVEN WHEN THEN
+    @Test
+    public void givenNewMeterWhenFirstAddictionThenProperCounter(){
+        //Given
+        ElectricityMeter electricityMeter = new ElectricityMeter();
+        //When
+        electricityMeter.addKwh(1);
+        //Then
+        Assert.assertTrue(electricityMeter.getKwh() == 1);
+    }
+
+
+}
